db3ef1968a6cce59136422791fcc3488eadbfd9d
Step 25 <bhuvanadeepika017@gmail.com>
2019-12-14 12:32:28 +0530

    added visit function and its test cases
    What changes you have made?
    added visit function and its test cases

--- test/pointTest.js
+++ test/pointTest.js
@@ -8,4 +8,17 @@ describe("class", function() {
       assert.strictEqual(point.toString(), "[Point @(2,3)]");
     });
   });
+
+  describe("visit", function() {
+    const point = new Point(2, 3);
+    it("should give the sum of the coordinates", function() {
+      const actual = point.visit((x, y) => x + y);
+      assert.strictEqual(actual, 5);
+    });
+
+    it("should give the product of the coordinates", function() {
+      const actual = point.visit((x, y) => x * y);
+      assert.strictEqual(actual, 6);
+    });
+  });
 });
