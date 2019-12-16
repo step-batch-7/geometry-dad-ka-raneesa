86bd4687ce4ad5f418fae5ed9c1e307f1313c833
Step 25 <bhuvanadeepika017@gmail.com>
2019-12-16 17:04:42 +0530

    modified findDistanceTo method in point class

--- src/point.js
+++ src/point.js
@@ -26,7 +26,7 @@ class Point {
   }
 
   findDistanceTo(point) {
-    if (!(point instanceof Point)) return NaN;
+    if (!(point instanceof Point)) return false;
     const dx = this.x - point.x;
     const dy = this.y - point.y;
     return Math.hypot(dx, dy);
